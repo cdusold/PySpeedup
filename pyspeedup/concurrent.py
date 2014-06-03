eea346e4b95116f57f389af1ced1b0404f390e46
Chris Dusold <chrisdusold@gmail.com>
2014-06-03 00:30:03 -0400

    Fixed up basic test and patched up concurrent classes for Windows.

--- pyspeedup/concurrent.py
+++ pyspeedup/concurrent.py
@@ -65,8 +65,19 @@ class Cache():
     def __repr__(self):
         return 'concurrent.Cache('+self.func.__repr__()+')'
         
-def _run(a_queue,a_generator):
+def _generatorFromResults(a_list,an_event):
+    i=0
+    while True:
+        try:
+            yield a_list[i]
+            i+=1
+        except:
+            an_event.wait(.1)
+    raise Exception("Deadlocked...")
+def _run(a_queue,a_gen_marshal,a_gen_name,a_list,an_event):
     try:
+        a_generator=_FunctionType(_loads(a_gen_marshal),globals(),"a_func")
+        globals()[a_gen_name]=_partial(_generatorFromResults,a_list,an_event)
         for each_value in a_generator():
             a_queue.put(each_value)
     except Exception as e:
@@ -75,10 +86,15 @@ class Buffer():
     "Current implementation requires the values be uniformly increasing (like the primes, or positive fibonnaci sequence)."
     def __init__(self,generator,buffersize=16,haltCondition=None):
         self._generator,self._buffersize=generator,buffersize
-        self._cache=[]
+        self._m=_Manager()
+        self._e=self._m.Event()
+        self._g=_dumps(generator.__code__)
+        self._n=generator.__name__
+        self._cache=self._m.list()
         #self.set_halt_condition(haltCondition) #This will make non-uniformly increasing generators usable without introducing a halting problem in the code (just in the userspace).
-        self._q=_Queue(self._buffersize)
-        self._thread=_Process(target=_run,args=(self._q,generator))
+        #self._q=_Queue(self._buffersize)
+        self._q=self._m.Queue(self._buffersize)
+        self._thread=_Process(target=_run,args=(self._q,self._g,self._n,self._cache,self._e))
         self._thread.daemon=True
         self._thread.start()
     def __del__(self):
@@ -104,10 +120,19 @@ class Buffer():
             return self._cache[key]
         else:
             while True:
-                self._cache.append(self._q.get(True,60))
-                cache_len+=1
-                if cache_len==key+1:
-                    return self._cache[key]
+                if self._q.empty():
+                    self._e.wait(.1)
+                    self._e.clear()
+                else:
+                    try:
+                        self._cache.append(self._q.get(True,10))
+                        cache_len+=1
+                        self._e.set()
+                        self._e.clear()
+                        if cache_len==key+1:
+                            return self._cache[key]
+                    except:
+                        print("Starts failing at {}. Manager debug info is {}.".format(cache_len, self._m._debug_info()))
     def pull_values(self):
         try:
             for i in range(self._buffersize):
@@ -123,7 +148,7 @@ def buffer(buffersize=16,haltCondition=None):
     return decorator
 
 def factor(N):
-    '''Runs multiple tests concurrently, starting with Monte Carlo primality testing, and ending with eliptic curves.'''
+    '''Runs multiple tests concurrently, starting with Monte Carlo primality testing, and ending with elliptic curves.'''
     #Ideally, a Prime-biased (Certificates) and Composite-biased (http://en.wikipedia.org/wiki/Baillie-PSW_primality_test) Monte Carlo test compete, and the first one to return a guaranteed answer stops the other.
     #Then brute-force prime(using concurrent buffer, likely, and a disk-complemented cache)<sqrt(N) checker for low hanging fruit, as they say...
     #Then start Pollard-Rho, so higher fruit...
@@ -132,72 +157,4 @@ def factor(N):
     #Then elliptic curves?
     #For each of these, if the montecarlo returns Prime IMMEDIATELY KILL, and return [1,N]
     #If any algorithm find a factor, kill the rest, and start factoring it and N divided by it concurrently.
-    pass            
-
-if __name__=='__main__':
-    @Cache
-    def _func(size,max,min):
-        '''A test function.'''
-        if size<min or max<min:
-            return 1 #Empty
-        if size<max:
-            return _func(size,size,min)
-        count=_func(size,max-1,min)
-        if max==size:
-            return count+1
-        for i,j in enumerate(range(size-max,-1,-1)):
-            count+= _func(i-1,max-1,min) * _func(j-1,max,min)
-        return count
-    def postponed_sieve():                   # postponed sieve, by Will Ness      
-        yield 2; yield 3; yield 5; yield 7;  # original code David Eppstein, 
-        D = {}                               #            ActiveState Recipe 2002
-        ps = (p for p in postponed_sieve())  # a separate Primes Supply:
-        p = next(ps) and next(ps)            # (3) a Prime to add to dict
-        q = p*p                              # (9) when its sQuare is 
-        c = 9                                # the next Candidate
-        while True:
-            if c not in D:                   # not a multiple of any prime seen so far:
-                if c < q: yield c            #   a prime, or
-                else:                        #   (c==q) next prime's square:
-                    add(D,c + 2*p,2*p)       #     (9+6,6 : 15,21,27,33,...)
-                    p=next(ps)               #     (5)
-                    q=p*p                    #     (25)
-            else:                            # a composite:
-                s = D.pop(c)                 #   step of increment
-                add(D,c + s,s)               #   next multiple, same step
-            c += 2                           # next odd candidate
-    @buffer()
-    def concurrent_sieve():                  # postponed sieve, by Will Ness
-        yield 2; yield 3; yield 5; yield 7;  # original code David Eppstein, 
-        D = {}                               #            ActiveState Recipe 2002
-        ps = (p for p in postponed_sieve())  # a separate Primes Supply:
-        p = next(ps) and next(ps)            # (3) a Prime to add to dict
-        q = p*p                              # (9) when its sQuare is 
-        c = 9                                # the next Candidate
-        while True:
-            if c not in D:                   # not a multiple of any prime seen so far:
-                if c < q: yield c            #   a prime, or
-                else:                        #   (c==q) next prime's square:
-                    add(D,c + 2*p,2*p)       #     (9+6,6 : 15,21,27,33,...)
-                    p=next(ps)               #     (5)
-                    q=p*p                    #     (25)
-            else:                            # a composite:
-                s = D.pop(c)                 #   step of increment
-                add(D,c + s,s)               #   next multiple, same step
-            c += 2                           # next odd candidate
-
-    def add(D,x,s):                          # make no multiple keys in Dict
-        while x in D: x += s                 # increment by the given step
-        D[x] = s    
-    primes=concurrent_sieve
-    for i in range(10):
-        print(str(primes[i]))
-    for i in range(20):
-        print(str(primes[i]))
-    defaultPrimer=postponed_sieve()
-    for i in range(100000):
-        assert next(defaultPrimer)==primes[i]
-    print("Success!")
-    del primes
-    print(str(_func(7,7,3)))
-    del _func
+    pass      
--- tests/cacheTests.py
+++ tests/cacheTests.py
@@ -1,4 +1,9 @@
 import unittest as ut
+from pyspeedup.algorithms import cached
+from pyspeedup._utils._timer import Timer
+from pyspeedup.concurrent import Cache
+from pyspeedup.concurrent import buffer
+from multiprocessing import freeze_support
 
 def uncachedFib(a):
     if a in [0,1]:
@@ -6,6 +11,16 @@ def uncachedFib(a):
     if a<0:
         raise Exception("Reverse fibonacci sequence not implemented.")
     return uncachedFib(a-1)+uncachedFib(a-2)
+
+class fibTest(ut.TestCase):
+    def test_fib(self):
+        self.assertEqual(uncachedFib(0),0,"The zeroth element of the Fibonnaci sequence is 0, not {}.".format(str(uncachedFib(0))))
+        self.assertEqual(uncachedFib(1),1,"The first element of the Fibonnaci sequence is 1, not {}.".format(str(uncachedFib(1))))
+        self.assertEqual(uncachedFib(2),1,"The second element of the Fibonnaci sequence is 1, not {}.".format(str(uncachedFib(2))))
+        self.assertEqual(uncachedFib(3),2,"The third element of the Fibonnaci sequence is 2, not {}.".format(str(uncachedFib(3))))
+        self.assertEqual(uncachedFib(4),3,"The fourth element of the Fibonnaci sequence is 3, not {}.".format(str(uncachedFib(4))))
+        self.assertEqual(uncachedFib(5),5,"The fifth element of the Fibonnaci sequence is 5, not {}.".format(str(uncachedFib(5))))
+        #self.assertRaises(f(-1)
 
 class cachedTest(ut.TestCase):
     c=None
@@ -52,5 +67,178 @@ class cachedTest(ut.TestCase):
         with Timer() as t1:
             k=self.c(32)
         self.assertGreater(t2.interval,t1.interval,"There isn't a speed up... This is useless then, I suppose.")
+        
 
-    #TODO: Test the concurrent cache.
+def fib(a): #Needs to be in the global scope.
+    if a in [0,1]:
+        return a
+    if a<0:
+        raise Exception("Reverse fibonacci sequence not implemented.")
+    fib.apply_async(a-1) #Also needs to prepare multiprocessed branching.
+    fib.apply_async(a-2)
+    return fib(a-1)+fib(a-2)
+        
+class CacheTest(ut.TestCase):
+    c=None
+    def setUp(self):
+        fib=Cache(globals()['fib'])
+        self.c=fib
+    def test_fib(self):
+        self.assertEqual(self.c(0),0,"The zeroth element of the Fibonnaci sequence is 0, not {}.".format(str(self.c(0))))
+        self.assertEqual(self.c(1),1,"The first element of the Fibonnaci sequence is 1, not {}.".format(str(self.c(1))))
+        self.assertEqual(self.c(2),1,"The second element of the Fibonnaci sequence is 1, not {}.".format(str(self.c(2))))
+        self.assertEqual(self.c(3),2,"The third element of the Fibonnaci sequence is 2, not {}.".format(str(self.c(3))))
+        self.assertEqual(self.c(4),3,"The fourth element of the Fibonnaci sequence is 3, not {}.".format(str(self.c(4))))
+        self.assertEqual(self.c(5),5,"The fifth element of the Fibonnaci sequence is 5, not {}.".format(str(self.c(5))))
+        #self.assertRaises(f(-1)
+    #def test_init(self): #TODO: redo for this.
+        #self.assertEqual(len(self.c._d),0,"The cache was malformed.")
+        #self.assertEqual(self.c.n,1,"The cache max size was not recorded properly.")
+        #self.assertEqual(self.c._f(0),uncachedFib(0),"The function was not entered correctly.")
+    def test_cache(self):
+        i=self.c(0)
+        self.assertEqual(len(self.c._d),1,"The value was not cached properly.")
+        self.assertEqual(self.c(0),i,"The cached answer was incorrect.")
+    #def test_pop(self): #TODO: pop? That's not a thing yet.
+        #i=self.c(3)
+        #self.assertEqual(len(self.c._d),3,"Recursion not properly set up for caching.")
+        #i=self.c(4)
+        #self.assertEqual(len(self.c.c),3,"Maximum cache size not implemented correctly.") #This feature doesn't exist (yet?)
+    def test_speed(self):
+        t1=None
+        t2=None
+        with Timer() as t1:
+            k=uncachedFib(32)
+        with Timer() as t2:
+            k=self.c(32)
+        #This test needs multiple cores to get a speed up, by nature.
+        #self.assertLess(t2.interval,t1.interval,"There isn't a speed up... This is useless then, I suppose.")
+        with Timer() as t1:
+            k=self.c(32)
+        self.assertGreater(t2.interval,t1.interval,"There isn't a speed up... This is useless then, I suppose.")
+#     def test_completion(self):
+#         self.assertEqual(_func(7,7,3),17,"_func(7,7,3) is 17 not {}".format(_func(7,7,3)))
+#         self.assertEqual(_func(50,50,3),16475640049,"_func(50,50,3) is 16475640049 not {}".format(_func(50,50,3)))
+
+@Cache
+def _func(size,mx,mn):
+    '''A test function.'''
+    if size<mn or mx<mn:
+        return 1 #Empty
+    if size<mx:
+        return _func(size,size,mn)
+    count=_func(size,mx-1,mn)
+    if mx==size:
+        return count+1
+    for i,j in enumerate(range(size-mx,-1,-1)):
+        _func.apply_async(i-1,mx-1,mn)
+        _func.apply_async(j-1,mx,mn)
+    for i,j in enumerate(range(size-mx,-1,-1)):
+        count+= _func(i-1,mx-1,mn) * _func(j-1,mx,mn)
+    return count
+
+class bufferTest(ut.TestCase):
+    c=None
+    def setUp(self):
+        self.primes=postponed_sieve()
+        self.mPrimes=concurrent_sieve
+    def test_primes(self):
+        p=[2,3,5,7,11,13,17,19,23,29,31,37,41,43]
+        for i in p:
+            self.assertEqual(next(self.primes),i,"The generator skipped {}.".format(i))
+    def test_mPrimes(self):
+        for i in range(100):
+            self.assertEqual(next(self.primes),concurrent_sieve[i],"The buffer skipped prime number {}.".format(i))
+    #def test_init(self): #TODO: redo for this.
+        #self.assertEqual(len(self.c._d),0,"The cache was malformed.")
+        #self.assertEqual(self.c.n,1,"The cache max size was not recorded properly.")
+        #self.assertEqual(self.c._f(0),uncachedFib(0),"The function was not entered correctly.")
+#     def test_cache(self):
+#         i=self.c(0)
+#         self.assertEqual(len(self.c._d),1,"The value was not cached properly.")
+#         self.assertEqual(self.c(0),i,"The cached answer was incorrect.")
+    #def test_pop(self): #TODO: pop? That's not a thing yet.
+        #i=self.c(3)
+        #self.assertEqual(len(self.c._d),3,"Recursion not properly set up for caching.")
+        #i=self.c(4)
+        #self.assertEqual(len(self.c.c),3,"Maximum cache size not implemented correctly.") #This feature doesn't exist (yet?)
+#     def test_speed(self):
+#         t1=None
+#         t2=None
+#         with Timer() as t1:
+#             k=uncachedFib(32)
+#         with Timer() as t2:
+#             k=self.c(32)
+#         #This test needs multiple cores to get a speed up, by nature.
+#         #self.assertLess(t2.interval,t1.interval,"There isn't a speed up... This is useless then, I suppose.")
+#         with Timer() as t1:
+#             k=self.c(32)
+#         self.assertGreater(t2.interval,t1.interval,"There isn't a speed up... This is useless then, I suppose.")
+#     def test_completion(self):
+#         self.assertEqual(_func(7,7,3),17,"_func(7,7,3) is 17 not {}".format(_func(7,7,3)))
+#         self.assertEqual(_func(50,50,3),16475640049,"_func(50,50,3) is 16475640049 not {}".format(_func(50,50,3)))
+        
+def postponed_sieve():                   # postponed sieve, by Will Ness      
+    yield 2; yield 3; yield 5; yield 7;  # original code David Eppstein, 
+    D = {}                               #            ActiveState Recipe 2002
+    ps = (p for p in postponed_sieve())  # a separate Primes Supply:
+    p = next(ps) and next(ps)            # (3) a Prime to add to dict
+    q = p*p                              # (9) when its sQuare is 
+    c = 9                                # the next Candidate
+    while True:
+        if c not in D:                   # not a multiple of any prime seen so far:
+            if c < q: yield c            #   a prime, or
+            else:                        #   (c==q) next prime's square:
+                add(D,c + 2*p,2*p)       #     (9+6,6 : 15,21,27,33,...)
+                p=next(ps)               #     (5)
+                q=p*p                    #     (25)
+        else:                            # a composite:
+            s = D.pop(c)                 #   step of increment
+            add(D,c + s,s)               #   next multiple, same step
+        c += 2                           # next odd candidate
+
+def concurrent_sieve():                  # postponed sieve, by Will Ness
+    yield 2; yield 3; yield 5; yield 7;  # original code David Eppstein, 
+    D = {}                               #            ActiveState Recipe 2002
+    ps = (p for p in concurrent_sieve())  # a separate Primes Supply:
+    p = next(ps) and next(ps)            # (3) a Prime to add to dict
+    q = p*p                              # (9) when its sQuare is 
+    c = 9                                # the next Candidate
+    while True:
+        if c not in D:                   # not a multiple of any prime seen so far:
+            if c < q: yield c            #   a prime, or
+            else:                        #   (c==q) next prime's square:
+                s = 2 * p                #     (9+6,6 : 15,21,27,33,...)
+                x = c + s                # make no multiple keys in Dict
+                while x in D: x += s     # increment by the given step
+                D[x] = s   
+                p=next(ps)               #     (5)
+                q=p*p                    #     (25)
+        else:                            # a composite:
+            s = D.pop(c)                 #   step of increment
+            x = c + s                    # make no multiple keys in Dict
+            while x in D: x += s         # increment by the given step
+            D[x] = s                     #   next multiple, same step
+        c += 2                           # next odd candidate
+concurrent_sieve=buffer()(concurrent_sieve)
+   
+def add(D,x,s):                          # make no multiple keys in Dict
+    while x in D: x += s                 # increment by the given step
+    D[x] = s   
+#        
+# primes=concurrent_sieve
+# for i in range(10):
+#     print(str(primes[i]))
+# for i in range(20):
+#     print(str(primes[i]))
+# defaultPrimer=postponed_sieve()
+# for i in range(100000): 
+#     assert next(defaultPrimer)==primes[i]
+# print("Success!")
+# del primes
+# print(str(_func(7,7,3)))
+# del _func
+
+if __name__=='__main__':
+    freeze_support()
+    ut.main()
