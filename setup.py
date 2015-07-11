a66c44316e66b99c93b5779602d8722d9dab6bb0
Chris Dusold (Poik) <chrisdusold@gmail.com>
2015-07-11 02:30:03 -0400

    Updated version number for bug fix.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "PySpeedup",
-    version = "0.1.2.0",
+    version = "0.1.2.1",
     author = "Chris Dusold",
     author_email = "PySpeedup@ChrisDusold.com",
     description = ("A multiprocess framework for efficient calculations."),
