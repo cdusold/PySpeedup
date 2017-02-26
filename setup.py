70f053b09b4b9f30f1e1df6a365a576dc65accf4
Chris Dusold <cdusold@vt.edu>
2017-02-26 00:06:52 -0500

    Forgot to update the version for the bug fix.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "PySpeedup",
-    version = "0.1.2.1",
+    version = "0.1.2.2",
     author = "Chris Dusold",
     author_email = "PySpeedup@ChrisDusold.com",
     description = ("A multiprocess framework for efficient calculations."),
